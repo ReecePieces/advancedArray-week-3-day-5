7307398c42497725431ede73c610c6d5d46514d6
Kaitlyn <kmiddlebrook2000@gmail.com>
2023-07-24 04:18:01 -0700

    16 Solution

--- problems/16-hipsterfy.js
+++ problems/16-hipsterfy.js
@@ -15,11 +15,28 @@ console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls
 */
 
 let removeLastVowel = function(word) {
-    // Your code here
+
+    let vowels = 'aeiou';
+
+    for(let i = word.length - 1; i >= 0; i--) {
+        let char = word[i];
+        if (vowels.includes(char)) {
+            return word.slice(0, i) + word.slice(i + 1);
+        }
+    }
+
+    return word;
 };
 
 let hipsterfy = function(sentence) {
-    // Your code here
+
+    let returnArr = [];
+    let sentArr = sentence.split(" ");
+
+    sentArr.forEach((word) => {
+        returnArr.push(removeLastVowel(word))
+    })
+    return returnArr.join(" ");
 };
 
 // alternative solution using Array.map
@@ -27,10 +44,14 @@ let hipsterfy = function(sentence) {
 //     return sentence.split(' ').map(removeLastVowel).join(' ');
 // };
 
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
     module.exports = hipsterfy;
 } catch (e) {
     module.exports = null;
-}
+}
